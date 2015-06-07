c15ccf3840d0ba82d105333b8a8212967af413dd
Steven Schubiger <stsc@refcnt.org>
2015-06-07 15:06:55 +0200

    Add release preparation script

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+source_file="colorize.c"
+man_file="colorize.1"
+echo "Setting version for $source_file"
+perl -i -pe 's/(?<=#define VERSION ")([^"]+)(?=")/$1+0.01/e' $source_file
+echo "Setting version for $man_file"
+perl -i -pe 's/(?<=\.TH COLORIZE 1 "\d{4}-\d{2}-\d{2}" "colorize v)([^"]+)(?=")/$1+0.01/e' $man_file
+exit 0
