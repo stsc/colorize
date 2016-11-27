bc475ef9585d31377b2f02dde4a4941ecec58d06
Steven Schubiger <stsc@refcnt.org>
2016-11-27 21:55:47 +0100

    Round version to 2 digits after decimal point

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 source_file="colorize.c"
 man_file="colorize.1"
 printf '%s\n' "Setting version for $source_file"
-perl -i -pe 's/(?<=#define VERSION ")([^"]+)(?=")/$1+0.01/e' $source_file
+perl -i -pe 's/(?<=#define VERSION ")([^"]+)(?=")/sprintf("%.2f",$1+0.01)/e' $source_file
 printf '%s\n' "Setting version for $man_file"
-perl -i -pe 's/(?<=\.TH COLORIZE 1 "\d{4}-\d{2}-\d{2}" "colorize v)([^"]+)(?=")/$1+0.01/e' $man_file
+perl -i -pe 's/(?<=\.TH COLORIZE 1 "\d{4}-\d{2}-\d{2}" "colorize v)([^"]+)(?=")/sprintf("%.2f",$1+0.01)/e' $man_file
 exit 0
